807c12c45c331b66bbda2ca7f6efaff1bb6fd76e
Gabrielmtn <Gabrielmtn@gmail.com>
2014-07-10 21:29:19 -0400

    pushing angular and SIBpad

---
+++ AngularTest/js/app.js
@@ -0,0 +1,14 @@
+(function() {
+    var app = angular.module('store', []);
+
+    app.controller('StoreController', function() {
+        this.product = gem
+    });
+
+    var gem = {
+        name: 'Dodecahedron',
+        price: 2.95,
+        description: '. . .',
+        canPurchase: true
+    };
+})();
