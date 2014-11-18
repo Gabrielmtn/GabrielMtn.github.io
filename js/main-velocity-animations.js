efc10916967d76530d7a90fcb6e91b8fafa08bf2
Gabrielmtn <Gabrielmtn@gmail.com>
2014-11-18 17:22:30 -0500

    adding home velocity animations

--- js/main-velocity-animations.js
+++ js/main-velocity-animations.js
@@ -1,8 +1,13 @@
-;$(function () {
+$(document).ready(function() {
   
-$("nav.navbar navbar-default navbar-fixed-top").velocity({rotateX:["0","-40"], scale:["1","0.99"],opacity:["1","0"] }, { duration: 800 }); 
-$("div.intro-lead-in").velocity({rotateX:["0","-14"], scale:["1","0.99"],opacity:["1","0"] }, { duration: 900, easing: "linear"  });
-$("div.intro-heading").velocity({rotateX:["0","-14"], scale:["1","0.99"],opacity:["1","0"] }, { duration: 950, easing: "linear"  });
+$("nav.navbar").velocity({rotateX:["0" , "-64"], scale:[ "1" , "0.96"],opacity:[ "1" , "0"] }, { duration: 900, easing : "linear"  }); 
+$("div.intro-lead-in").velocity({rotateX:["0" , "-14"], scale:[ "1" , "0.99"],opacity:[ "0" , "0"] }, { duration: 550, delay: 0, easing : "linear"  });
+$("div.intro-lead-in").velocity({rotateX:["0" , "-14"], scale:[ "1" , "0.99"],opacity:[ "1" , "0"] }, { duration: 900, delay: 550, easing : "linear"  });
 
+
+$("div.intro-heading").velocity({rotateX:[ "0" , "-14"], scale:[ "1" , "0.99"],opacity:[ "0" , "0"] }, { duration: 550, delay: 0, easing : "linear"  });
+$("div.intro-heading").velocity({rotateX:[ "0" , "-14"], scale:[ "1" , "0.99"],opacity:[ "1" , "0"] }, { duration: 950, delay: 550, easing : "linear"  });
+
+$(".btn-xl").velocity({rotateX:[ "0" , "-14"], scale:[ "1" , "0.99"],opacity:[ "0" , "0"] }, { duration: 750, delay: 0, easing : "linear"  });
+$(".btn-xl").velocity({rotateX:[ "0" , "-14"], scale:[ "1" , "0.99"],opacity:[ "1" , "0"] }, { duration: 950, delay: 750, easing : "linear"  });
 });
-
