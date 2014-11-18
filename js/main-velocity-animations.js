f7929e358f2d4db907f5a85717a8fa1ee070e57b
Gabrielmtn <Gabrielmtn@gmail.com>
2014-11-18 16:56:03 -0500

    initial velocity additions

---
+++ js/main-velocity-animations.js
@@ -0,0 +1,8 @@
+;$(function () {
+  
+$("nav.navbar navbar-default navbar-fixed-top").velocity({rotateX:["0","-40"], scale:["1","0.99"],opacity:["1","0"] }, { duration: 800 }); 
+$("div.intro-lead-in").velocity({rotateX:["0","-14"], scale:["1","0.99"],opacity:["1","0"] }, { duration: 900, easing: "linear"  });
+$("div.intro-heading").velocity({rotateX:["0","-14"], scale:["1","0.99"],opacity:["1","0"] }, { duration: 950, easing: "linear"  });
+
+});
+
