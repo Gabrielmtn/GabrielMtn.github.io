c4c9c1ea05288a72c7e6d13fb80795085facd3bf
Gabrielmtn <Gabrielmtn@gmail.com>
2014-11-18 19:56:26 -0500

    velocity add

--- js/main-velocity-animations.js
+++ js/main-velocity-animations.js
@@ -1,17 +1,29 @@
 $(document).ready(function() {
   
 $("nav.navbar").velocity({rotateX:["0" , "-64"], scale:[ "1" , "0.96"],opacity:[ "1" , "0"] }, { duration: 900, easing : "linear"  }); 
-$("div.intro-lead-in").velocity({rotateX:["0" , "-14"], scale:[ "1" , "0.99"],opacity:[ "0" , "0"] }, { duration: 550, delay: 0, easing : "linear"  });
+
+// $("div.intro-lead-in").velocity({color:"#ffffff" }, { duration: 10, delay: 0, easing : "linear"  });
+$("div.intro-lead-in").velocity({rotateX:["0" , "-14"],color:"#eee", scale:[ "1" , "0.99"],opacity:[ "0" , "0"] }, { duration: 550, delay: 0, easing : "linear"  });
 $("div.intro-lead-in").velocity({rotateX:["0" , "-14"], scale:[ "1" , "0.99"],opacity:[ "1" , "0"] }, { duration: 900, delay: 550, easing : "linear"  });
 
 
-$("div.intro-heading").velocity({rotateX:[ "0" , "-14"], scale:[ "1" , "0.99"],opacity:[ "0" , "0"] }, { duration: 550, delay: 0, easing : "linear"  });
+// $("div.intro-heading").velocity({color:"#ffffff" }, { duration: 10, delay: 0, easing : "linear"  });
+$("div.intro-heading").velocity({rotateX:[ "0" , "-14"], color:"#eee", scale:[ "1" , "0.99"],opacity:[ "0" , "0"] }, { duration: 550, delay: 0, easing : "linear"  });
 $("div.intro-heading").velocity({rotateX:[ "0" , "-14"], scale:[ "1" , "0.99"],opacity:[ "1" , "0"] }, { duration: 950, delay: 550, easing : "linear"  });
 
-$(".btn-xl").velocity({rotateX:[ "0" , "-14"], scale:[ "1" , "0.99"],opacity:[ "0" , "0"] }, { duration: 750, delay: 0, easing : "linear"  });
+
+
+
+
+
+
+$(".btn-xl").velocity({rotateX:[ "0" , "-14"], backgroundColor: "#fed136",borderColor: "#fed136",color: "#eee",opacity:[ "0" , "0"] }, { duration: 750, delay: 0, easing : "linear"  });
 $(".btn-xl").velocity({rotateX:[ "0" , "-14"], scale:[ "1" , "0.99"],opacity:[ "1" , "0"] }, { duration: 950, delay: 750, easing : "linear"  });
 
 
 $("#habla_window_state_div").velocity({rotateX:[ "0" , "-14"], scale:[ "1" , "0.99"],opacity:[ "0" , "0"] }, { duration: 1550, delay: 0, easing : "linear"  });
 $("#habla_window_state_div").velocity({rotateX:[ "0" , "-14"], scale:[ "1" , "0.99"],opacity:[ "1" , "0"] }, { duration: 2950, delay: 1550, easing : "linear"  });
+
+
+
 });
