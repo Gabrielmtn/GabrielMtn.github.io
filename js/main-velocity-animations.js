c0086295bb0c37a0ec39add1675aa7889ee2a3cf
Gabriel Martin <gabriel@MacBook-Pro.home>
2014-12-14 16:26:04 -0500

    built a introjs demo to share with the wellist team 8x

--- js/main-velocity-animations.js
+++ js/main-velocity-animations.js
@@ -21,17 +21,16 @@ $('.navbar-default .nav li a').velocity({color:"#fff"},{ duration: 5, delay:15, 
 // #  ██║██║ ╚████║   ██║   ██║  ██║╚██████╔╝       ██║   ███████╗██╔╝ ██╗   ██║   
 // #  ╚═╝╚═╝  ╚═══╝   ╚═╝   ╚═╝  ╚═╝ ╚═════╝        ╚═╝   ╚══════╝╚═╝  ╚═╝   ╚═╝   
 // #                                                                               
-$("section#me").velocity({backgroundPositionX:"40%",backgroundPositionY:"0%"}, { delay: 0, easing : "linear"  })
+$("section#me").velocity({backgroundPositionX:"40%",backgroundPositionY:"0%"}, { duration: 4000, delay: 0, easing : "linear"  })
 .velocity({backgroundPositionX:"61%",backgroundPositionY:"100%"}, { duration: 4480, delay: 0, easing : "linear"  })
 .velocity({backgroundPositionX:"40%",backgroundPositionY:"0%"}, { duration: 5480, delay: 0, easing : "linear"  })
 .velocity({backgroundPositionX:"61%",backgroundPositionY:"100%"}, { duration: 4480, delay: 2000, easing : "linear"  })
-.velocity({backgroundPositionX:"40%",backgroundPositionY:"0%"}, { duration: 4000, delay: 0, easing : "linear"  });
+.velocity({backgroundPositionX:"40%",backgroundPositionY:"0%"}, { duration: 000, delay: 0, easing : "linear"  });
 
 // $("div.intro-lead-in").velocity({color:"#ffffff" }, { duration: 10, delay: 0, easing : "linear"  });
 $("div.intro-lead-in").velocity({rotateX:["0" , "-14"],color:"#fff", scale:[ "1" , "0.99"],opacity:[ "0" , "0"] }, { duration: 480, delay: 0, easing : "linear"  });
 $("div.intro-lead-in").velocity({translateY:"-12",rotateX:["0" , "-14"], scale:[ "1" , "0.99"],opacity:[ "1" , "0"] }, { duration: 1000, delay: 300, easing : "linear"  });
 
-$("#me").velocity({translateY:[ "-200" , "0"] }, { duration: 0, delay: 0, easing : "linear"  });
 $("header").velocity({translateY:[ "-100" , "0"] }, { duration: 300, delay: 0, easing : "linear"  });
 $("header").velocity({color:"#000",opacity:[ "0" , "0"] }, { duration: 480, delay: 0, easing : "linear"  });
 $("header").velocity({opacity:[ "1" , "0"] }, { duration: 800, delay: 480, easing : "linear"  });
